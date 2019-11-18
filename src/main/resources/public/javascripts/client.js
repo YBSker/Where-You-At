9627cdc71e28d00460a49d27a728d7bc13afe954
mattachea <matthewchea@yahoo.com>
2019-11-18 16:26:02 -0500

    first commit

--- src/main/resources/public/javascripts/client.js
+++ src/main/resources/public/javascripts/client.js
@@ -1,9 +1,10 @@
+
 class Application extends React.Component {
     render() {
         return (
             <div>
-                {/*<NavigationBar/>*/}
-                <h1> Where you at </h1>
+                <NavigationBar/>
+
             </div>
         );
     }
