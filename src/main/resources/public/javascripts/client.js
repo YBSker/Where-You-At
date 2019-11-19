dec00ea147c8ae3dcaa29a422616b45243dc626c
mattachea <matthewchea@yahoo.com>
2019-11-18 22:06:39 -0500

    navbar, settings, sidebar, events front end works

--- src/main/resources/public/javascripts/client.js
+++ src/main/resources/public/javascripts/client.js
@@ -1,12 +1,108 @@
+const SIDEBAR_STATE = {
+    closed: 0,
+    event: 1,
+    cardList: 2,
+    settings: 3,
+};
+
+const mainContainerStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'auto 400px',
+    height: '100vh'
+};
+
+const mainContainerClosedStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'auto',
+    height: '100vh'
+};
 
 class Application extends React.Component {
-    render() {
-        return (
-            <div>
-                <NavigationBar/>
+    constructor(props) {
+        super(props);
+        this.state = {
+            sidebarState: SIDEBAR_STATE.closed
+        };
 
-            </div>
-        );
+        this.updateSidebar = this.updateSidebar.bind(this);
+    }
+
+    updateSidebar(state) {
+        this.setState({sidebarState: state});
+    }
+
+    render() {
+        switch (this.state.sidebarState) {
+            case SIDEBAR_STATE.closed:
+                return (
+                    <div>
+                        <NavigationBar updateSidebar = {this.updateSidebar}/>
+                        <div className="mainContainer" style={mainContainerClosedStyle}>
+                            <div className="mapContainer">
+                                <GoogleMap updateSidebar = {this.updateSidebar}/>
+                            </div>
+                        </div>
+                    </div>
+                );
+
+            case SIDEBAR_STATE.event:
+                return (
+                    <div>
+                        <NavigationBar updateSidebar = {this.updateSidebar}/>
+                        <div className="mainContainer" style={mainContainerStyle}>
+                            <div className="mapContainer">
+                                <GoogleMap updateSidebar = {this.updateSidebar}/>
+                            </div>
+                            <div className="side">
+                                <EventForm/>
+                            </div>
+                        </div>
+                    </div>
+                );
+
+            case SIDEBAR_STATE.cardList:
+                return (
+                    <div>
+                        <NavigationBar updateSidebar = {this.updateSidebar}/>
+                        <div className="mainContainer" style={mainContainerStyle}>
+                            <div className="mapContainer">
+                                <GoogleMap updateSidebar = {this.updateSidebar}/>
+                            </div>
+                            <div className="side">
+                                <Sidebar/>
+                            </div>
+                        </div>
+                    </div>
+                );
+
+            case SIDEBAR_STATE.settings:
+                return (
+                    <div>
+                        <NavigationBar updateSidebar = {this.updateSidebar}/>
+                        <div className="mainContainer" style={mainContainerStyle}>
+                            <div className="mapContainer">
+                                <GoogleMap updateSidebar = {this.updateSidebar}/>
+                            </div>
+                            <div className="side">
+                                <Settings/>
+                            </div>
+                        </div>
+                    </div>
+                );
+
+
+            default:
+                return (
+                    <div>
+                        <NavigationBar updateSidebar = {this.updateSidebar}/>
+                        <div className="mainContainer" style={mainContainerClosedStyle}>
+                            <div className="mapContainer">
+                                <GoogleMap updateSidebar = {this.updateSidebar}/>
+                            </div>
+                        </div>
+                    </div>
+                );
+        }
     }
 }
 
