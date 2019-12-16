e317077aa6e1d820f0336eb2a039b4519433d961
Nick Xitco <nxitco1@jhu.edu>
2019-12-16 14:41:55 -0500

    Cleaned up Event Details

--- src/main/resources/public/javascripts/components/EventDetails.js
+++ src/main/resources/public/javascripts/components/EventDetails.js
@@ -29,36 +29,35 @@ class EventDetails extends React.Component {
         const eventInfo = this.props.eventInfo;
         console.log(eventInfo);
         console.log(eventInfo.name);
-        const eventName = eventInfo.name;
         return (
             <div className="side">
                 <div className="side-header">
                     <h1 className="side-header-text">Event Details</h1>
                 </div>
 
-                <div className="event-card">
-                    <div className="event-name">
-                        <h2 className="event-name-text">{eventInfo.name}</h2>
-                    </div>
+                <div className="event-list">
+                    <div className="event-card">
+                        <div className="event-name">
+                            <h2 className="event-name-text">{eventInfo.name}</h2>
+                        </div>
 
-                    <div className="event-details">
-                        <p>Description: <b>{eventInfo.description}</b></p>
-                        <p>Address: <b>{eventInfo.place}</b></p>
-                        <p>Start Time: <b>{eventInfo.startTime}</b></p>
+                        <div className="event-details">
+                            <p>Description: <b>{eventInfo.description}</b></p>
+                            <p>Address: <b>{eventInfo.place}</b></p>
+                            <p>Start Time: <b>{eventInfo.startTime}</b></p>
 
-                        <div>
-                            {/*<DropdownButton title = "Add Friends" onSelect={(e) => {alert(e + " added to Event")}}>*/}
-                            <DropdownButton title = "Add Friends" onSelect={(e) => {this.handleSelect(eventInfo.identifier, e)}}>
+                            <div>
+                                {/*<DropdownButton title = "Add Friends" onSelect={(e) => {alert(e + " added to Event")}}>*/}
+                                <DropdownButton title = "Add Friends" onSelect={(e) => {this.handleSelect(eventInfo.identifier, e)}}>
 
-                                {this.state.myFriends.map(person => (
-                                    <Dropdown.Item as="button" eventKey={person.identifier}> {person.fullName} </Dropdown.Item>
-                                ))}
-                            </DropdownButton>
+                                    {this.state.myFriends.map(person => (
+                                        <Dropdown.Item as="button" eventKey={person.identifier}> {person.fullName} </Dropdown.Item>
+                                    ))}
+                                </DropdownButton>
+                            </div>
                         </div>
                     </div>
                 </div>
-
-
             </div>
         );
     }
