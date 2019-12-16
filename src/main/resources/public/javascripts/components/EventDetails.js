c0503ccd668a6412a15b5aa60338cad5c83cef06
Nick Xitco <nxitco1@jhu.edu>
2019-12-16 12:58:20 -0500

    Set up for profile pictures and adjusted styling on EventDetails

--- src/main/resources/public/javascripts/components/EventDetails.js
+++ src/main/resources/public/javascripts/components/EventDetails.js
@@ -1,3 +1,6 @@
+//TODO You could merge this file and Your Events if you add a conditional on the way the function is accessed,
+//and then map all events in the array to event cards, whether it's one or many
+
 class EventDetails extends React.Component {
     constructor(props) {
         super(props);
@@ -8,22 +11,23 @@ class EventDetails extends React.Component {
         const eventInfo = this.props.eventInfo;
         console.log(eventInfo);
         console.log(eventInfo.name);
-        const eventName = eventInfo.name;
         return (
             <div className="side">
                 <div className="side-header">
                     <h1 className="side-header-text">Event Details</h1>
                 </div>
 
-                <div className="event-card">
-                    <div className="event-name">
-                        <h2 className="event-name-text">{eventInfo.name}</h2>
-                    </div>
+                <div className="event-list">
+                    <div className="event-card">
+                        <div className="event-name">
+                            <h2 className="event-name-text">{eventInfo.name}</h2>
+                        </div>
 
-                    <div className="event-details">
-                        <p>Description: <b>{eventInfo.description}</b></p>
-                        <p>Address: <b>{eventInfo.place}</b></p>
-                        <p>Start Time: <b>{eventInfo.startTime}</b></p>
+                        <div className="event-details">
+                            <p>Description: <b>{eventInfo.description}</b></p>
+                            <p>Address: <b>{eventInfo.place}</b></p>
+                            <p>Start Time: <b>{eventInfo.startTime}</b></p>
+                        </div>
                     </div>
                 </div>
             </div>
