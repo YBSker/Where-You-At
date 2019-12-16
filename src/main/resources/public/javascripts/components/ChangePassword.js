56ccebf0153471ee4a91fcb722c803f1e7f7c7f9
YBSker <stanley.chu@live.com>
2019-12-15 19:34:02 -0500

    added change apss page

---
+++ src/main/resources/public/javascripts/components/ChangePassword.js
@@ -0,0 +1,16 @@
+class ChangePassword extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return(
+            <div>
+                <header>
+                    <h1> Hello World!</h1>
+                </header>
+            </div>
+        )
+    }
+}
